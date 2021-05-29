4e5230b346be7534f5a40c238b1fdf90d07eca2c
caparcom <>
2021-05-29 17:06:20 +0000

    Added 1.2a-c

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,37 +3,34 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-const readlineSync = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = "";
+let correctAnswers = "";
+let candidateAnswers = "";
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = readlineSync.question("Hello. Please input your first and last name: ")
+candidateName = input.question("Hello. Please input your first and last name: ")
 };
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
-}
+candidateAnswer = input.question("Who was the first American woman in space? ");
+};
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
-  
-
-  return grade;
+  if (candidateAnswer === correctAnswer) {
+  console.log("That is correct! Sally Ride was the first American Woman in space. ");
+  } else {
+    console.log("I'm sorry but that is incorrect. Sally Ride was the first American Woman in space. ");
+  }
 }
 
 function runProgram() {
