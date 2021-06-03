4292305755ad396916c1ce82af23590d90aba03d
caparcom <>
2021-06-03 17:57:17 +0000

    all 1.2 assignments complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,16 +1,17 @@
 const input = require('readline-sync');
-
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = {firstName : "", lastName : ""} ;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = "";
-let correctAnswers = "";
-let candidateAnswers = "";
+let questions = ['1. Who was the first American woman in space?', '2. True or false: 5 kilometer == 5000 meters?',	
+'3. (5 + 3)/2 * 10 = ?', "4. Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", 	
+'5. What is the minimum crew size for the ISS?']
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -20,17 +21,43 @@ candidateName = input.question("Hello. Please input your first and last name: ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question("Who was the first American woman in space? ");
+  for (let i = 0; i<questions.length; i++) {
+    candidateAnswers.push(input.question(`${questions[i]} \n\t`));
+  }
+  console.log("One moment please. Commencing grading protocol. ")
 };
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer) {
-  console.log("That is correct! Sally Ride was the first American Woman in space. ");
-  } else {
-    console.log("I'm sorry but that is incorrect. Sally Ride was the first American Woman in space. ");
+  console.log('\n-------GRADING PROTOCOL ENACTED-------\n\n')
+  let numCorrect = 0;
+  let total = 5;
+  for (let i = 0; i<candidateAnswers.length; i++) {
+      console.log(`\nQuestion ${questions[i]} \n\tYour Answer: ${candidateAnswers[i]}\n\tCorrect Answer: ${correctAnswers[i]} `);
+      if (candidateAnswers[i] === correctAnswers[i]) {
+      console.log("CORRECT");
+      numCorrect += 1;
+      } else if (candidateAnswers.toString().toLowerCase()===correctAnswers.toString().toLowerCase()) {
+       console.log("CORRECT");
+       numCorrect += 1;
+      }  /*else if (candidateAnswers.toString().toUpperCase()===correctAnswers.toString().toUpperCase()) {
+            console.log("CORRECT");
+            numCorrect += 1;*/
+      else {
+      console.log("FALSE");
+
+      }
+      
   }
+  
+  let grade = (numCorrect / total * 100);
+  if (grade >= 80) {
+    console.log(`Congratulations! You've passed the assessment with a final score of ${grade}%`);
+  } else {
+      console.log(` We regret to inform you that a score of ${grade}% does not constitute as passing. `);
+    }
+
 }
 
 function runProgram() {
@@ -40,6 +67,13 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+
+
+
+
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
